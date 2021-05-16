95ff60564eecb627bf6af15cbddf76487b481e43
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-16 17:50:21 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,12 +1,35 @@
 
 def encrypt(key,plaintext):
     ciphertext=""
-    #YOUR CODE HERE
+    message = plaintext.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ciphertext = ""
+
+    for letter in message:
+        if letter in alpha: #if the letter is actually a letter
+            #find the corresponding ciphertext letter in the alphabet
+            letter_index = (alpha.find(letter) + key) % len(alpha)
+
+            ciphertext = ciphertext + alpha[letter_index]
+        else:
+            ciphertext = ciphertext + letter
+
     return ciphertext
 
 def decrypt(key,ciphertext):
-    plaintext=""
-    #YOUR CODE HERE
+    message = ciphertext.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    plaintext = ""
+
+    for letter in message:
+        if letter in alpha: #if the letter is actually a letter
+            #find the corresponding ciphertext letter in the alphabet
+            letter_index = (alpha.find(letter) - key) % len(alpha)
+
+            plaintext = plaintext + alpha[letter_index]
+        else:
+            plaintext = plaintext + letter
+
     return plaintext
 
 
