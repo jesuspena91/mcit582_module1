7a33ff49cd2d57099c48900fa3c88560a0d68173
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-16 17:41:38 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,7 +1,19 @@
 
 def encrypt(key,plaintext):
     ciphertext=""
-    #YOUR CODE HERE
+
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+ 
+        # Encrypt uppercase characters
+        if (char.isupper()):
+            ciphertext += chr((ord(char) + s-65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            ciphertext += chr((ord(char) + s - 97) % 26 + 97)
+ 
     return ciphertext
 
 def decrypt(key,ciphertext):
