72c7fba71f42c9d2f5e8740f83654295871bf53e
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-16 17:54:40 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,32 +1,27 @@
 
 def encrypt(key,plaintext):
-    ciphertext=""
-    message = plaintext.upper()
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    plaintext = plaintext.upper()
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     ciphertext = ""
 
-    for letter in message:
-        if letter in alpha: #if the letter is actually a letter
-            #find the corresponding ciphertext letter in the alphabet
-            letter_index = (alpha.find(letter) + key) % len(alpha)
-
-            ciphertext = ciphertext + alpha[letter_index]
+    for letter in plaintext:
+        if letter in alphabet:
+            letter_index = (alphabet.find(letter) + key) % 26
+            ciphertext = ciphertext + alphabet[letter_index]
         else:
             ciphertext = ciphertext + letter
 
     return ciphertext
 
 def decrypt(key,ciphertext):
-    message = ciphertext.upper()
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ciphertext = ciphertext.upper()
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     plaintext = ""
 
-    for letter in message:
-        if letter in alpha: #if the letter is actually a letter
-            #find the corresponding ciphertext letter in the alphabet
-            letter_index = (alpha.find(letter) - key) % len(alpha)
-
-            plaintext = plaintext + alpha[letter_index]
+    for letter in ciphertext:
+        if letter in alphabet:
+            letter_index = (alphabet.find(letter) - key) % 26
+            plaintext = plaintext + alphabet[letter_index]
         else:
             plaintext = plaintext + letter
 
